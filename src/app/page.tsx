2eea5b5ddc1ed80567344191a4574d5cc0907002
saiffardin <saiffardin@gmail.com>
2025-01-29 14:00:54 +0600

    add shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
@@ -47,6 +48,8 @@ export default function Home() {
           >
             Read our docs
           </a>
+
+          <Button variant={"destructive"}>Click me</Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
